83e999bf3090f3f284a0621b8a500a64573bd6c6
ansrl0107 <ansrl0107@gmail.com>
2024-11-04 20:07:35 +0900

    feat: 각 entity의 id 값을 value class 를 사용하도록 변경

--- src/main/kotlin/com/mkroo/sparrow/domain/Profile.kt
+++ src/main/kotlin/com/mkroo/sparrow/domain/Profile.kt
@@ -4,9 +4,9 @@ import org.springframework.data.relational.core.mapping.Table
 
 @Table("profile")
 data class Profile(
-    val accountId: Long,
+    val accountId: AccountId,
     val name: String,
     val description: String
 ) : AbstractTable() {
-    constructor(account: Account, name: String, description: String) : this(account.id, name, description)
+    constructor(account: Account, name: String, description: String) : this(AccountId(account.id), name, description)
 }
--- src/main/kotlin/com/mkroo/sparrow/repository/ProfileRepository.kt
+++ src/main/kotlin/com/mkroo/sparrow/repository/ProfileRepository.kt
@@ -1,12 +1,11 @@
 package com.mkroo.sparrow.repository
 
-import com.mkroo.sparrow.domain.Account
+import com.mkroo.sparrow.domain.AccountId
 import com.mkroo.sparrow.domain.Profile
-import org.springframework.data.r2dbc.repository.Query
+import com.mkroo.sparrow.domain.ProfileId
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import reactor.core.publisher.Flux
 
-interface ProfileRepository : ReactiveCrudRepository<Profile, Long> {
-    @Query("SELECT * FROM profile WHERE account_id = :account")
-    fun findByAccount(account: Account): Flux<Profile>
+interface ProfileRepository : ReactiveCrudRepository<Profile, ProfileId> {
+    fun findByAccountId(accountId: AccountId): Flux<Profile>
 }
